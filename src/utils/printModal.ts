ba35d142fad32924ef8bcb1b8541bb7c50d27aa7
Marijn Bent <marijn@marijnbent.nl>
2024-09-01 13:32:03 +0200

    remove style from print modal

--- src/utils/printModal.ts
+++ src/utils/printModal.ts
@@ -1,41 +1,17 @@
 import { PrintPluginSettings } from '../types';
-import { readFileSync } from 'fs';
 import { Printd } from 'printd'
 
-export async function openPrintModal(content: HTMLElement, settings: PrintPluginSettings, pluginStylePath: string, userStylePath: string): Promise<void> {
+/**
+ * Generate the HTML with the content to be printed. Use Printd to print.
+ * 
+ * @param content 
+ * @param settings 
+ * @param cssString 
+ * @returns 
+ */
+export async function openPrintModal(content: HTMLElement, settings: PrintPluginSettings, cssString: string): Promise<void> {
     return new Promise((resolve) => {
 
-        /**
-         * The CSS is loaded from the two paths. Both are optional. 
-         */
-        let pluginStyle;
-        let userStyle;
-
-        try {
-            pluginStyle = readFileSync(pluginStylePath, 'utf8') ?? ''
-        } catch { }
-
-        try {
-            userStyle = readFileSync(userStylePath, 'utf8') ?? ''
-        } catch { }
-
-        const cssString = `
-            body { 
-                font-size: ${settings.fontSize};
-            }
-            h1, .inline-title { font-size: ${settings.h1Size}; }
-            h2 { font-size: ${settings.h2Size}; }
-            h3 { font-size: ${settings.h3Size}; }
-            h4 { font-size: ${settings.h4Size}; }
-            h5 { font-size: ${settings.h5Size}; }
-            h6 { font-size: ${settings.h6Size}; }
-            ${pluginStyle ?? ''}
-            ${userStyle ?? ''}
-        `;
-
-        /**
-         * Create the HTML
-         */
         const htmlElement = document.createElement('html');
         const headElement = document.createElement('head');
 

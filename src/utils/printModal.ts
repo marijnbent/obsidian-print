fc608fe2d67980c2b9d9214a5c8b5933328b7585
Marijn Bent <marijn@marijnbent.nl>
2024-09-27 12:48:18 +0200

    Update printModal.ts

--- src/utils/printModal.ts
+++ src/utils/printModal.ts
@@ -16,17 +16,48 @@ export async function openPrintModal(content: HTMLElement, settings: PrintPlugin
     const titleElement = document.createElement('title');
     titleElement.textContent = 'Print note';
     headElement.appendChild(titleElement);
+
+    if (settings.debugMode) {
+        const styleElement = document.createElement('style');
+        styleElement.textContent = cssString;
+        headElement.appendChild(styleElement);
+    }
+
     htmlElement.appendChild(headElement);
 
     const bodyElement = document.createElement('body');
     bodyElement.className = 'obsidian-print';
     bodyElement.appendChild(content);
 
-    if (settings.debugMode) {
-        console.log(content);
-    }
-
     htmlElement.appendChild(bodyElement);
+
+    /**
+     * This uses Electron to open a window with HTML content in order to inspect it when debug mode is turned on.
+     */
+    if (settings.debugMode) {
+        const { remote } = (window as any).require("electron");
+
+        let printWindow = new remote.BrowserWindow({
+            width: 800,
+            height: 600,
+            show: true,
+            webPreferences: {
+                nodeIntegration: true,
+                contextIsolation: false
+            }
+        });
+
+        /**
+         * This uses outerHTML solely when debug mode is turned on to make it easier to inspect the generated HTML
+         * and CSS stylying. For debuggers: Press `cmd/ctrl + p` in the DevTools and search for 'Emulate CSS Print media type'
+         */
+        const debugContent = htmlElement.outerHTML;
+        printWindow.loadURL(`data:text/html;charset=utf-8,${encodeURIComponent(debugContent)}`);
+
+        printWindow.webContents.on('did-finish-load', () => {
+            printWindow.webContents.openDevTools();
+        });
+    }
 
     const d = new Printd()
     d.print(htmlElement, [cssString])
